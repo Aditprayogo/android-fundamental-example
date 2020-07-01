1b002a6bb3f92ddb8c78949007312deb6a076447
aditPrayogo <adit.ihzar@gmail.com>
2020-07-01 16:43:17 +0700

    adding MyWebView

---
+++ MyWebView/app/src/main/java/com/example/mywebview/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.mywebview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.JsResult
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView.loadUrl("https://www.dicoding.com")
+
+        webView.settings.javaScriptEnabled = true
+
+        webView.webViewClient = object : WebViewClient() {
+            override fun onPageFinished(view: WebView, url: String) {
+                Toast.makeText(this@MainActivity, "Web Dicoding berhasil dimuat", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient(){
+            override fun onJsAlert(
+                view: WebView?,
+                url: String?,
+                message: String?,
+                result: JsResult?
+            ): Boolean {
+                Toast.makeText(this@MainActivity, message, Toast.LENGTH_LONG).show()
+                result?.confirm()
+                return true
+            }
+        }
+    }
+}
